7a1cfd58cb7fca421d5ad375bfc7f95288eb0e61
ChiKien_Luu <luukien2910@gmail.com>
2026-01-20 17:06:00 +0700

    feat: added feature create Student logic and some helpful method in  J1.L.P0021

--- J1.L.P0021/StudentManagement/src/controller/StudentController.java
+++ J1.L.P0021/StudentManagement/src/controller/StudentController.java
@@ -1,9 +1,58 @@
 package controller;
 
+import model.CourseType;
+import model.Student;
+import utility.DataHelper;
+import utility.Validation;
+
+import java.util.ArrayList;
+
 /**
  * @author Chi Kien-Luu | github/luwukien
  * Initialize Date: 19/01/2026
  */
 
 public class StudentController {
+    private final DataHelper dataHelper = new DataHelper();
+    private final Validation validation = new Validation();
+    private final int LEAST_STUDENT_NUMBER = 10;
+
+    /**
+     *
+     * @param listStudent
+     * @return
+     */
+    public ArrayList<Student> createStudent(ArrayList<Student> listStudent) {
+        while(true) {
+            System.out.println("====== Create New Student ======");
+            String id = dataHelper.inputString("Enter id:");
+            String name = dataHelper.inputString("Enter student name: ");
+
+            if (!validation.isNameMatchWithId(listStudent, id, name)) {
+                System.err.println("The id exist in the list Student. Please input other id!!!");
+                continue;
+            }
+
+            String semester = dataHelper.inputString("Enter semester: ");
+            CourseType course = dataHelper.inputCourseName("Enter course: ");
+
+            if (validation.isDuplicate(listStudent, id, name, semester, course)) {
+                System.err.println("The student duplicated record. Please try again!!!");
+                continue;
+            }
+
+            listStudent.add(new Student(id, name, semester, course));
+            System.out.println("Added successfully the student");
+
+            if (listStudent.size() >= LEAST_STUDENT_NUMBER) {
+                boolean choice = dataHelper.getYesNoChoice("Do you want to continue (Y/N)?",
+                        "You can only enter Y/y or N/n. Please try again!!!");
+                if (!choice) {
+                    break;
+                }
+            }
+        }
+        return listStudent;
+
+    }
 }
--- J1.L.P0021/StudentManagement/src/utility/Validation.java
+++ J1.L.P0021/StudentManagement/src/utility/Validation.java
@@ -1,11 +1,57 @@
 package utility;
 
+import model.CourseType;
+import model.Student;
+
+import java.util.ArrayList;
+
 /**
  * @author Chi Kien-Luu | github/luwukien
  * Initialize Date: 19/01/2026
  */
 
 public class Validation {
+
+    /**
+     *
+     * @param listStudent
+     * @param id
+     * @param name
+     * @param courseType
+     * @param semester
+     * @return
+     */
+    public boolean isDuplicate(ArrayList<Student> listStudent, String id, String name,
+                               String semester, CourseType courseType) {
+
+        for (Student student : listStudent) {
+            if (student.getStudentName().equalsIgnoreCase(name)
+                    && student.getId().equalsIgnoreCase(id)
+                    && student.getCourseName().equals(courseType)
+                    && student.getSemester().equalsIgnoreCase(semester)) return true;
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     * @param listStudent
+     * @param id
+     * @param name
+     * @return
+     */
+    public boolean isNameMatchWithId(ArrayList<Student> listStudent, String id, String name) {
+        for (Student student : listStudent) {
+            if (student.getId().equalsIgnoreCase(id)) {
+                if (student.getStudentName().equalsIgnoreCase(name)) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 }
