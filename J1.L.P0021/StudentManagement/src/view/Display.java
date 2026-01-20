45e0af2f53ed9022cf4fd7b3c2ec472c868b30b1
ChiKien_Luu <luukien2910@gmail.com>
2026-01-20 17:03:30 +0700

    update: Display.java in J1.L.P0021

--- J1.L.P0021/StudentManagement/src/view/Display.java
+++ J1.L.P0021/StudentManagement/src/view/Display.java
@@ -1,9 +1,24 @@
 package view;
 
+import utility.DataHelper;
+
 /**
  * @author Chi Kien-Luu | github/luwukien
  * Initialize Date: 19/01/2026
  */
 
 public class Display {
+    private final DataHelper validation = new DataHelper();
+
+    public int displayMenu() {
+        System.out.println("============== Student Management Program ==============");
+        System.out.println(" 1.	Create");
+        System.out.println(" 2.	Find and Sort");
+        System.out.println(" 3.	Update/Delete");
+        System.out.println(" 4.	Report");
+        System.out.println(" 5.	Exit");
+
+        int choice = validation.checkInputLimitChoices("Enter your choice",1, 4);
+        return choice;
+    }
 }
