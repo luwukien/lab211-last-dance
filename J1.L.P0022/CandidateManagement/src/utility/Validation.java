b3b168242e2467d8e55a62a793100b387e8085c8
ChiKien_Luu <luukien2910@gmail.com>
2026-01-29 23:20:24 +0700

    update feature: check duplicate id

--- J1.L.P0022/CandidateManagement/src/utility/Validation.java
+++ J1.L.P0022/CandidateManagement/src/utility/Validation.java
@@ -1,9 +1,29 @@
 package utility;
 
+import model.Candidate;
+
+import java.util.ArrayList;
+
 /**
  * @author Chi Kien Luu | github.com/luwukien
  * Initialize Date: 27/01/2026
  */
 public class Validation {
 
+    /**
+     *
+     * @param listCandidate
+     * @param idCandidate
+     * @return
+     */
+    public boolean checkIdCandidate(ArrayList<Candidate> listCandidate, String idCandidate) {
+        boolean isExist = false;
+        for (Candidate candidate : listCandidate) {
+            if (candidate.getId().equalsIgnoreCase(idCandidate)) {
+                isExist = true;
+                break;
+            }
+        }
+        return isExist;
+    }
 }
